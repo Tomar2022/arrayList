3f947871f65ca11dda7b190befcfa7bd18487b23
tomar2022 <tomarjrobinson@gmail.com>
2022-09-28 18:27:49 -0400

    arrayList

---
+++ Arrays.java
@@ -0,0 +1,41 @@
+package arrayList;
+import java.util.ArrayList;
+
+
+public class Arrays {
+
+	public static void main(String[] args) {
+	
+
+		String[] fruits = new String[5];
+		fruits[0] = "Apple";
+		
+		
+		ArrayList fruitList = new ArrayList();
+		fruitList.add("Apple");
+		fruitList.add("Orange");
+		fruitList.add("Watermelon");
+		fruitList.add("Mango");
+		fruitList.add("Kiwi");
+		
+	System.out.println(fruitList);
+	
+	fruitList.remove("Mango");
+	fruitList.remove("Kiwi");
+	System.out.println(fruitList);
+	
+	
+		
+				
+				
+
+
+		
+		
+	
+		
+		
+
+	}
+
+}
